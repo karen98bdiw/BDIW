23df21b1d6bf224beccb3b6d1557ef94d038f0e6
karen98bdiw <ghazaryan.deb@mail.ru>
2019-08-03 00:11:14 +0400

    putting home activity(users list) into fragment view

--- app/src/main/java/com/example/bdiw/Activities/SignInActivity.kt
+++ app/src/main/java/com/example/bdiw/Activities/SignInActivity.kt
@@ -36,7 +36,7 @@ class SignInActivity : AppCompatActivity() {
                             startActivity(intent)
 
                         }else{
-                            Log.e("signIn","noSign")
+                            Log.e("signIn","noSignin")
                         }
                     })
             }
